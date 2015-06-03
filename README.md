02434233d2fbb3903b12322a09da7f0a95122aca
Paweł Stradowski <p.stradowski@gmail.com>
2015-06-03 22:45:07 +0200

    updated Readme and code moved to function

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This is simple Lua script to use Sharp GP2Y1010AU0F with NodeMCU.
 It is inspired by work of Chris Nafis http://www.howmuchsnow.com/arduino/airquality/ and https://github.com/Trefex/arduino-airquality/tree/master/Module_Dust-Sensor, but I used more precise approach to calibrate the sensor.
-While Chris Nafis had a chance to compare, I must rather use an approach described in the application note: http://sharp-world.com/products/device/lineup/data/pdf/datasheet/gp2y1010au_appl_e.pdf - I store minimum value reported by the sensor in a config file (storage.lua) and calculate final measurements useing V0 as described in the applicaiton note.
+While Chris Nafis had a chance to compare his sensor with Dylos DC1100 Pro Air Quality, I must rather use an approach described in the application note: http://sharp-world.com/products/device/lineup/data/pdf/datasheet/gp2y1010au_appl_e.pdf - I store minimum value reported by the sensor in a config file (storage.lua) and calculate final measurements using V0 as described in the applicaiton note.
 I use a voltage divider to lower down the output of GP2Y1010AU0F to the level of ESP8266(1V), so the code incorporates values of my resistors.
 
--- sharp.lua
+++ sharp.lua
@@ -1,26 +1,27 @@
-file.open("storage.lua", "r")
-v0=file.readline()
-v0=v0*1
-file.close()
-
-led_pin=5
-gpio.mode(led_pin,gpio.OUTPUT)
-gpio.write(led_pin,gpio.HIGH)
-tmr.delay(10000)
-gpio.write(led_pin,gpio.LOW)
-tmr.delay(280)
-voltage=adc.read(0)
-tmr.delay(40)
-gpio.write(led_pin,gpio.HIGH)
-print(voltage)
-if voltage<v0 then
-    v0=voltage
-    file.open("storage.lua", "w+")
-    file.writeline(v0)
+function ReadSharp()
+    file.open("storage.lua", "r")
+    v0=file.readline()
+    v0=tonumber(v0)
     file.close()
+    led_pin=5
+    gpio.mode(led_pin,gpio.OUTPUT)
+    gpio.write(led_pin,gpio.HIGH)
+    tmr.delay(10000)
+    gpio.write(led_pin,gpio.LOW)
+    tmr.delay(280)
+    voltage=adc.read(0)
+    tmr.delay(40)
+    gpio.write(led_pin,gpio.HIGH)
+    print(voltage)
+    if voltage<v0 then
+        v0=voltage
+        file.open("storage.lua", "w+")
+        file.writeline(v0)
+        file.close()
+    end
+    voltage=(voltage/1024)*3.615384615
+    v0=(v0/1024)*3.615384615
+    dust=(voltage*0.17-v0)*1000
+    print("Sharp output:"..dust.."ug/m3")
+    return dust
 end
-voltage=(voltage/1024)*3.615384615
-v0=(v0/1024)*3.615384615
-dust=(voltage*0.17-v0)*1000
-print("Sharp output:"..dust.."ug/m3")
-
